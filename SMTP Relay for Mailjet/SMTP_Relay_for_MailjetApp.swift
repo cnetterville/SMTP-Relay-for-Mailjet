95a2ec13426cf6d18af0390e8473e723c1a75c55
Curtis Netterville <curtis@netterville.com>
2026-05-09 11:01:39 -0500

    Initial Commit

---
+++ SMTP Relay for Mailjet/SMTP_Relay_for_MailjetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMTP_Relay_for_MailjetApp.swift
+//  SMTP Relay for Mailjet
+//
+//  Created by Curtis Netterville on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct SMTP_Relay_for_MailjetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
